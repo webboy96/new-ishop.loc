fc98962a9f33e8ed0d463b93eba02f3251ce187e
rinatpc <rinat2511@gmail.com>
2022-05-13 14:06:09 +0500

    Main CONSTs and composer added

--- public/index.php
+++ public/index.php
@@ -1,1 +1,7 @@
-<h1>Привет</h1>
+<?php
+if (PHP_MAJOR_VERSION < 8)
+{
+    die('Необходима версия PHP >= 8');
+}
+require_once dirname(__DIR__) . '/config/init.php';
+echo ROOT;
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'c251fc9335a6abed654f6b8b1ca5fc8ade57f72d',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'c251fc9335a6abed654f6b8b1ca5fc8ade57f72d',
+            'dev_requirement' => false,
+        ),
+    ),
+);
