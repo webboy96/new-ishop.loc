21d34350a19aaf0a9d7eac43262538be13deb9fe
rinatpc <rinat2511@gmail.com>
2022-05-24 16:26:37 +0500

    DB add

--- app/views/layouts/ishop.php
+++ app/views/layouts/ishop.php
@@ -5,9 +5,19 @@ use wfm\View;
 /** @var $this View */
 
 ?>
-<?php $this->getPart('parts/headers'); ?>
+<?php $this->getPart('parts/header'); ?>
 <?=
 // выводим все что между открывающим и закрывающим тегом
 $this->content;
 ?>
 <?php $this->getPart('parts/footer'); ?>
+
+<?php
+$array1 = ["a" => "green", "pink", "blue", "red", "brown", "r" => "red", "red"];
+$array2 = ["b" => "green", "brown", "black", "yellow", "pink",];
+$result = array_keys(array_diff($array1, $array2));
+debug($result);
+$string = '123foo';
+$der = (int) $string;
+echo int('123foo');
+?>
