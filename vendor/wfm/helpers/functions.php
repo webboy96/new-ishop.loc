02b1d71c13d4cc03dd0278cce1fc44d3484060d2
rinatpc <rinat2511@gmail.com>
2022-06-10 16:03:04 +0500

    Add language route

--- vendor/wfm/helpers/functions.php
+++ vendor/wfm/helpers/functions.php
@@ -13,3 +13,17 @@ function h($str)
 {
     return htmlspecialchars($str);
 }
+
+function redirect($http = false)
+{
+    if ($http)
+    {
+        $redirect  = $http;
+    }
+    else
+    {
+        $redirect = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER']: PATH;
+    }
+    header("Location: $redirect");
+    die;
+}
