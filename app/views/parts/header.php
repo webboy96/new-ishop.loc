bc57f060e338f399a9ad2bebaac669daa1ddeee5
rinatpc <rinat2511@gmail.com>
2022-06-15 16:59:36 +0500

    Add Dynamic Cache Main Menu

--- app/views/parts/header.php
+++ app/views/parts/header.php
@@ -7,18 +7,18 @@ use wfm\View;
 <!doctype html>
 <html lang="en">
 <head>
-    <!-- Required meta tags -->
-    <base href="<?= base_url(); ?>">
+    <base href="<?= base_url() ?>">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="<?= PATH ?>/assets/bootstrap/css/bootstrap.min.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css" integrity="sha384-vSIIfh2YWi9wW0r9iZe7RJPrKwp6bG+s9QZMoITbCckVJqGCCRhc+ccxNcdpHuYu" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css"
+          integrity="sha384-vSIIfh2YWi9wW0r9iZe7RJPrKwp6bG+s9QZMoITbCckVJqGCCRhc+ccxNcdpHuYu" crossorigin="anonymous">
     <link rel="stylesheet" href="<?= PATH ?>/assets/css/magnific-popup.css">
     <link rel="stylesheet" href="<?= PATH ?>/assets/css/main.css">
     <link rel="icon" type="image/png" sizes="32x32" href="<?= PATH ?>/assets/img/favicon.png">
-    <?= $this->getMeta(); ?>
+    <?= $this->getMeta() ?>
 </head>
 <body>
 
@@ -34,7 +34,7 @@ use wfm\View;
                 <div class="col text-end icons">
                     <form>
                         <div class="input-group" id="search">
-                            <input type="text" class="form-control" placeholder="<?php __('tpl_search') ?>" name="s">
+                            <input type="text" class="form-control" placeholder="<?php __('tpl_search'); ?>" name="s">
                             <button class="btn close-search" type="button"><i class="fas fa-times"></i></i></button>
                             <button class="btn" type="submit"><i class="fas fa-search"></i></button>
                         </div>
@@ -53,13 +53,12 @@ use wfm\View;
                             <i class="far fa-user"></i>
                         </a>
                         <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#"><?php __('tpl_login') ?></a></li>
-                            <li><a class="dropdown-item" href="#"><?php __('tpl_signup') ?></a></li>
+                            <li><a class="dropdown-item" href="#">Авторизация</a></li>
+                            <li><a class="dropdown-item" href="#">Регистрация</a></li>
                         </ul>
                     </div>
 
                     <?php new \app\widgets\language\Language() ?>
-
 
                 </div>
             </div>
@@ -71,13 +70,19 @@ use wfm\View;
 
             <nav class="navbar navbar-expand-lg navbar-light">
                 <div class="container-fluid p-0">
-                    <a class="navbar-brand" href="<?= base_url(); ?>"><?= \wfm\App::$app->getProperty('site_name') ?></a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <a class="navbar-brand" href="<?= base_url() ?>"><?= \wfm\App::$app->getProperty('site_name') ?></a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
 
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <?php new \app\widgets\menu\Menu([
+                            'class' => 'navbar-nav ms-auto mb-2 mb-lg-0',
+                            'cache' => 30,
+                        ]) ?>
+                        <!--<ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                             <li class="nav-item">
                                 <a class="nav-link" href="category.html">Компьютеры</a>
                             </li>
@@ -99,7 +104,7 @@ use wfm\View;
                             <li class="nav-item">
                                 <a class="nav-link" href="category.html">Камеры</a>
                             </li>
-                        </ul>
+                        </ul>-->
                     </div>
 
                 </div>
