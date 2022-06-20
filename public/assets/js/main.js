48f289bb0c40c2224824f66c574c5a28a620196c
rinatpc <rinat2511@gmail.com>
2022-06-20 15:48:14 +0500

    Add Cart Button Clear Cart and Delete Item actions on click

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -7,6 +7,13 @@ $(function() {
 		const myModalEl = document.querySelector('#cart-modal');
 		const modal = bootstrap.Modal.getOrCreateInstance(myModalEl);
 		modal.show();
+		if ($('.cart-qty').text())
+		{
+			$('.count-items').text($('.cart-qty').text());
+	} else
+		{
+			$('.count-items').text('0');
+		}
 	}
 
 	$('#get-cart').on('click', function(e) {
@@ -23,6 +30,37 @@ $(function() {
 
 
 	})
+
+	$('#cart-modal .modal-cart-content').on('click', '#clear-cart', function(e) {
+		$.ajax({
+			url: 'cart/clear',
+			type: 'GET',
+			success: function (res) {
+				showCart(res);
+			},
+			error: function () {
+				alert('Error!');
+			}
+		});
+
+	});
+
+	$('#cart-modal .modal-cart-content').on('click', '.del-item', function(e) {
+		e.preventDefault;
+		const id = $(this).data('id');
+		$.ajax({
+			url: 'cart/delete',
+			type: 'GET',
+			data: {id: id},
+			success: function (res) {
+				showCart(res);
+			},
+			error: function () {
+				alert('Error!');
+			}
+		});
+
+	});
 
 	$('.add-to-cart').on('click', function (e) {
 		e.preventDefault();
